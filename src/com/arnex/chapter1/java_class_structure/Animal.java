00bafd23b7be11c5ad30f7f598cb20d03c2cbfab
Arnold Paye <arnoldpayet@gmail.com>
2020-08-06 08:54:30 -0400

    Add field and methdos to Animal.java.

--- src/com/arnex/chapter1/java_class_structure/Animal.java
+++ src/com/arnex/chapter1/java_class_structure/Animal.java
@@ -1,5 +1,13 @@
 package com.arnex.chapter1.java_class_structure;
 
 public class Animal {
-
+    // field (variable)
+    String name;
+    // methods (procedures, functions)
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
 }
